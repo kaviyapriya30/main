330a35ac455893b2d0e912208e680f7215bb316a
kaviya <kaviya@kaviya-VirtualBox>
2022-01-10 10:35:43 +0530

    second commit

--- example/src/example1/Exam.java
+++ example/src/example1/Exam.java
@@ -4,7 +4,8 @@ public class Exam {
 
 	public static void main(String[] args) {
 		System.out.println("kaviya");
-
+        System.out.println("jeeva");
+        System.out.println("Mohan");
 	}
 
 }
